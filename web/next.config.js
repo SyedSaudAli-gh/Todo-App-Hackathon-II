6b36b5df19d885831ba746257cce907165641218
SyedSaudAli-gh <saud.saleem93@gmail.com>
2026-01-20 18:36:34 +0500

    set the vercel build logs error

--- web/next.config.js
+++ web/next.config.js
@@ -1,12 +1,20 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   compress: true,
   env: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000',
-    NEXT_PUBLIC_API_VERSION: process.env.NEXT_PUBLIC_API_VERSION || 'v1',
+    NEXT_PUBLIC_API_BASE_URL:
+      process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000",
+    NEXT_PUBLIC_API_VERSION: process.env.NEXT_PUBLIC_API_VERSION || "v1",
   },
-}
+  webpack: (config) => {
+    // 🔹 Alias for @/ to point to src/
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
